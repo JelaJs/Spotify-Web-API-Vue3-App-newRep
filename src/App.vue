fb2d819f2b1badc108e4d7ce01f945b6e938f3f3
JelaJs <nikola4434@gmail.com>
2024-01-11 14:49:54 +0100

    Fixed bugs and some features

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       For the app to work properly, after authorization, make sure to keep the original Spotify
       open, and to set the proper audio device active(play random song)
     </p>-->
-    <p>Also for some features, like playing songs, you will need Spotify premium</p>
+    <p>For some features, like playing songs, you will need Spotify premium</p>
   </div>
   <div class="grid" v-else>
     <div class="responsiveMenuToggler">
